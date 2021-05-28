0ad6abc74251882215bb8420e9246542a752ae95
usman15idris <84938461+usman15idris@users.noreply.github.com>
2021-05-29 00:39:20 +0100

    Add files via upload

---
+++ firstbank usman.py
@@ -0,0 +1,26 @@
+def menu():
+    print("welcome to FIRST bank")
+    print("1. balance")
+    print("2. transfer")
+    print("3. Exit")
+    option=input()
+    if option == "1":   
+        print("balance is 2000")
+        menu()
+    elif option =="2":
+         Amount = input("enter account number you want to transfer money")
+amount = input("enter amount")
+pin = input("enter four digit")
+confirm = input("do you want to end") 
+    print("\n1. yes \n2. no")
+    if confirm == ("1")
+        print("transaction successful")
+        print("enter to return to menu \n1. end \n2. end")
+            print("Balance is N7000")
+            menu()
+            if option =="3":
+                exit
+                print("an sms will be sent to you shortly")
+            else:
+                print("invalid input")
+menu()
